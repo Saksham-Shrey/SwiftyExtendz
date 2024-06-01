b9df4dfebd3196e4f76c7fbc5fc7e129f01f8282
Saksham Shrey <sakshamshrey_work@icloud.com>
2024-06-01 19:02:08 +0530

    removed demo app

--- Example/Pods/Target Support Files/Pods-SwiftyExtendz_Example/Pods-SwiftyExtendz_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftyExtendz_Example : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftyExtendz_Example
-@end
--- Example/Pods/Target Support Files/Pods-SwiftyExtendz_Tests/Pods-SwiftyExtendz_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftyExtendz_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftyExtendz_Tests
-@end
